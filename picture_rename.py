9d2d1c6c45cbcb916e6f40107f5f89518a5e6852
bilginez <ebb88@cornell.edu>
2024-06-07 19:09:56 -0400

    Add picture rename tool

---
+++ picture_rename.py
@@ -0,0 +1,35 @@
+import os
+import argparse
+import shutil
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--folder', help='Path to images', default='./uploads/user_post')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+
+    args = get_parser()
+
+    print(f'Old Directory is {args.folder}')
+
+    new_folder = args.folder + '_renamed'
+
+    print(f'New Directory is {new_folder}')
+
+
+    os.makedirs(new_folder, exist_ok=True)
+
+    filelist = os.listdir(args.folder)
+
+    print(filelist)
+
+    for filename in filelist:
+        name, ext = filename.rsplit('_', 1)
+        new_filename = f'{name}.{ext}'
+
+        print(os.path.join(args.folder, filename), os.path.join(new_folder, new_filename))
+
+        shutil.copy(os.path.join(args.folder, filename), os.path.join(new_folder, new_filename))
+        
