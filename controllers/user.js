2084e1d1b3897302cf554eb89302d7ecaa79056e
bilginez <ebb88@cornell.edu>
2024-12-03 16:40:30 -0500

    Minor change

--- controllers/user.js
+++ controllers/user.js
@@ -75,6 +75,7 @@ exports.logout = async(req, res) => {
             req.session.destroy((err) => {
                 if (err) console.log('Error : Failed to destroy the session during logout.', err);
                 req.user = null;
+                alert(endSurveyLink);
                 res.redirect(endSurveyLink);
             });
         });
